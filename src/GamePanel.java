806cd6b817e4f0823600bdc55835275ae54162f9
Salhi-Walid <walid.dev@outlook.fr>
2021-12-20 01:22:01 +0100

    Added a score text on the game panel & game over text

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -47,41 +47,167 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g){
-        for(int i =0; i<SCREEN_HEIGHT/UNIT_SIZE;i++){
-            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
-            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
 
+        if(running){
+            /* This draws a grid to better design the game */
+//            for(int i =0; i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+//                g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+//                g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+//            }
+
+            /* It draws an apple on the screen randomly */
+            g.setColor(Color.red);
+            g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+
+            /* it draws the snake body parts */
+            for(int i = 0; i< bodyParts;i++){
+                if(i==0){
+                    g.setColor(Color.green);
+                    g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+                }
+                else{
+                    g.setColor(new Color(45,180,0));
+                    g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+                }
+            }
+            // IT DISPLAYS THE SCORE TEXT
+            g.setColor(Color.red);
+            g.setFont(new Font("Ink Free", Font.BOLD,35));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Pommes mangées: "+applesEaten,(SCREEN_WIDTH - metrics.stringWidth("Pommes mangées: "+applesEaten))/2,g.getFont().getSize());
         }
+        else{
+            gameOver(g);
+        }
+
+
     }
 
     public void newApple(){
-
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
 
     public void move(){
+        for(int i = bodyParts;i>0;i--){
+            x[i]=x[i-1];
+            y[i]=y[i-1];
+        }
 
+        switch (direction){
+            case 'U':
+                y[0]=y[0] - UNIT_SIZE;
+                break;
+
+            case 'D':
+                y[0]=y[0] + UNIT_SIZE;
+                break;
+
+            case 'L':
+                x[0]=x[0] - UNIT_SIZE;
+                break;
+
+            case 'R':
+                x[0]=x[0] + UNIT_SIZE;
+                break;
+        }
     }
 
     public void checkApple(){
-
+        if((x[0] == appleX) && (y[0] == appleY)){
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
     }
     public void checkCollisions(){
+        // THIS CHECK IF HEAD COLLIDES WITH BODY
+        for(int i = bodyParts; i > 0; i--){
+            if((x[0]==x[i])&&(y[0]==y[i])){
+                running=false;
+            }
+        }
+
+        //THIS CHECK IF THE HEAD TOUCHES THE LEFT BORDER
+        if(x[0] < 0){
+            running = false;
+        }
+
+        //THIS CHECK IF THE HEAD TOUCHES THE RIGHT BORDER
+        if(x[0] > SCREEN_WIDTH){
+            running = false;
+        }
+        //THIS CHECK IF THE HEAD TOUCHES THE TOP BORDER
+        if(y[0] < 0){
+            running = false;
+        }
+
+        //THIS CHECK IF THE HEAD TOUCHES THE BOTTOM BORDER
+        if(y[0] > SCREEN_HEIGHT){
+            running = false;
+        }
+
+        if(!running){
+            timer.stop();
+        }
 
     }
 
     public void gameOver(Graphics g){
 
+        //GAME OVER TEXT
+        g.setColor(Color.green);
+        g.setFont(new Font("Ink Free", Font.BOLD,75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over !",(SCREEN_WIDTH - metrics.stringWidth("Game Over !"))/2,SCREEN_HEIGHT/2);
+
+        //SCORE TEXT
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD,35));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Pommes mangées: "+applesEaten,(SCREEN_WIDTH - metrics2.stringWidth("Pommes mangées: "+applesEaten))/2,g.getFont().getSize());
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(running){
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e){
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R'){
+                        direction = 'L';
+                    }
 
+                    break;
+
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L'){
+                        direction = 'R';
+                    }
+                    break;
+
+                case KeyEvent.VK_UP:
+                    if(direction != 'D'){
+                        direction = 'U';
+                    }
+                    break;
+
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U'){
+                        direction = 'D';
+                    }
+                    break;
+            }
         }
     }
 }
